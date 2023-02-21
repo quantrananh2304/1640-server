1952030ec22e8b39f6f05b9877d32967d917e80c
Quan Tran <65522530+quantrananh2304@users.noreply.github.com>
2023-02-21 12:12:58 +0700

    ideas collection

---
+++ src/app-repositories/models/common/Idea.ts
@@ -0,0 +1,133 @@
+import { Schema, Types, model } from "mongoose";
+import { USER_COLLECTION_NAME } from "./User";
+
+export const IDEA_COLLECTION_NAME = "Ideas";
+
+export interface IdeaModelInterface {
+  title: string;
+  description: string;
+  like: Array<{
+    user: string | Types.ObjectId;
+    createdAt: Date;
+  }>;
+  dislike: Array<{
+    user: string | Types.ObjectId;
+    createdAt: Date;
+  }>;
+  views: Array<{
+    user: string | Types.ObjectId;
+    createdAt: Date;
+  }>;
+  createdAt: Date;
+  updatedAt: Date;
+  updatedBy: string | Types.ObjectId;
+  comments: Array<{
+    content: string;
+    createdBy: string | Types.ObjectId;
+    createdAt: Date;
+    editHistory: Array<{
+      content: string;
+      updatedAt: Date;
+    }>;
+  }>;
+  documents: Array<string>;
+}
+
+const ideaSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    default: "",
+  },
+  description: {
+    type: String,
+    required: true,
+    default: "",
+  },
+  like: [
+    {
+      type: {
+        user: {
+          type: Types.ObjectId,
+          ref: USER_COLLECTION_NAME,
+        },
+        createdAt: Date,
+      },
+      default: [],
+      required: true,
+    },
+  ],
+  dislike: [
+    {
+      type: {
+        user: {
+          type: Types.ObjectId,
+          ref: USER_COLLECTION_NAME,
+        },
+        createdAt: Date,
+      },
+      default: [],
+      required: true,
+    },
+  ],
+  views: [
+    {
+      type: {
+        user: {
+          type: Types.ObjectId,
+          ref: USER_COLLECTION_NAME,
+        },
+        createdAt: Date,
+      },
+      default: [],
+      required: true,
+    },
+  ],
+  comments: [
+    {
+      type: {
+        content: String,
+        createdBy: {
+          type: Types.ObjectId,
+          ref: USER_COLLECTION_NAME,
+        },
+        createdAt: Date,
+        editHistory: [
+          {
+            type: {
+              content: String,
+              updatedAt: Date(),
+            },
+            default: [],
+          },
+        ],
+      },
+      required: true,
+      default: [],
+    },
+  ],
+  documents: [
+    {
+      type: String,
+      required: true,
+      default: [],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    required: true,
+    default: new Date(),
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: new Date(),
+  },
+  updatedBy: {
+    type: Types.ObjectId,
+    required: true,
+    ref: USER_COLLECTION_NAME,
+  },
+});
+
+export default model<IdeaModelInterface>(IDEA_COLLECTION_NAME, ideaSchema);
